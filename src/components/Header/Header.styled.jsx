4cb73016846c514af69b27e4ecfb91b1524c182f
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-12-05 20:55:03 +0200

    Added snowman background

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -7,12 +7,11 @@ export const HeaderBox = styled.header`
   position: sticky;
   z-index: 1100;
   display: flex;
-  /* justify-content: space-between; */
   align-items: center;
   min-height: 64px;
   padding: 0 40px;
   color: #fff;
-  background-color: #360560;
+  background-color: #0c007e;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
